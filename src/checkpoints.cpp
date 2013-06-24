35098063e0e4eff45fde916ce9d017749dd2e3f5
Joseph Hopkins <neucoin@gmail.com>
2013-06-24 16:13:10 -0600

    Changed some of the visuals and added checkpoints

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -27,6 +27,9 @@ namespace Checkpoints
         (         0, uint256("0xd592e6110f92acc63899d45070221eaa5d75ec23950387d3d7595f6a61d178b2"))
         (        50, uint256("0x53f893c43e4a94d3350ed03cf30da535e8e14109db262ea31dd79d6f5f5cd022"))
         (       100, uint256("0x33b4eeb1abd2fe62d0c400c17fd3fc0adc89e8803c8a74cdc1efc5f9299c393b"))
+		(      4000, uint256("0x3dd24e2bc22c030ceb0b4f85d6ca0b6d028ba80e25ac64da7b17f3db41917912"))
+		(      4500, uint256("0x31aa7baf89ac8aae7ec7cd46a85602f3c3c9f77260f77e133ddd37db83a0200b"))
+		(      5000, uint256("0x4b26ad750625d438e9455d909a91b6481924ea7d49ccc2a0626cbf1697256e0d"))
         ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
